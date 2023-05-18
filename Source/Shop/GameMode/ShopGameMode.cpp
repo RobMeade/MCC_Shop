46f3847bed9dbe024a983ab40dcd0bef555933c1
Rob <baldy.meade@gmail.com>
2023-05-18 10:41:21 +0100

    Update Project Content Structure

--- Source/Shop/GameMode/ShopGameMode.cpp
+++ Source/Shop/GameMode/ShopGameMode.cpp
@@ -7,7 +7,7 @@
 AShopGameMode::AShopGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Shop/PlayerCharacter/BP_ShopPlayerCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
